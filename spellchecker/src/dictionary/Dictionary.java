819e1b5a9b30f11275779d154ce224c473a3badc
Nicolás Bonader <neb0113@famaf.unc.edu.ar>
2015-05-22 13:01:42 -0300

    dict

--- spellchecker/src/dictionary/Dictionary.java
+++ spellchecker/src/dictionary/Dictionary.java
@@ -1,24 +1,35 @@
 package dictionary;
 import java.util.List;
-
-import word.Word;
+import word.*;
 
 public abstract class Dictionary {
+	
+	protected WordSet mySet; 
 
 	public void dictionary() {
+		mySet = new WordSet();
 	}
 
 	public void add(Word w) {
+		mySet.add(w);
 	}
 
 	public boolean contains(Word w) {
-		return true;
+		return mySet.contains(w);
 	}
 	
 	public void clear() {
+		mySet.clear();
 	}
 	
 	public void fromStringList(List <String> str) {
+		
+		Word w;
+		
+		for(int i = 0; i < str.size(); i++){
+			w = new Word(str.get(i));
+			this.add(w);
+		}
 	}
 
 	public List <String> toStringList() {
@@ -26,7 +37,7 @@ public abstract class Dictionary {
 	}
 	
 	public int size() {
-		return 0;
+		return mySet.size();
 	}
 
 }
