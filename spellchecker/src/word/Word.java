0b449026a5b3ecf78c8f4789ea8e62aa9f63951c
Melisa Bonias <mel.bonias@gmail.com>
2015-05-23 15:22:32 -0300

    word y wordset listos

--- spellchecker/src/word/Word.java
+++ spellchecker/src/word/Word.java
@@ -6,33 +6,45 @@ public class Word {
 	
 	public Word() {
 		word = new String();
-		setWord("");
 	}
 
 	public Word(String w) {
 		word = new String();
 		setWord(w);
 	}
-	
-	public void setWord(String w) {
-		word = w;
-	}
-	
-	public String getWord() {
-		return word;	
+
+	 public String getWord() {
+		return word;
 	}
 
-	 @Override
-	 public int hashCode() {
-		 
-		 return 0;
-		 
-	 }
-	 
-	 @Override
-	 public boolean equals(Object obj) {
-		return (this == obj);
-		 
-	 }	 
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
+
 	 
 }
--- spellchecker/src/word/WordSet.java
+++ spellchecker/src/word/WordSet.java
@@ -1,27 +1,32 @@
 package word;
 
-import java.util.Iterator;
+import java.util.*;
 
 public class WordSet {
-
+	
+	private Set<Word> wordSet;
+	
 	public WordSet() {
+		wordSet = new HashSet<Word>();
 	}
 
 	public void add(Word w) {
+		wordSet.add(w);
 	}
 
 	public boolean contains(Word w) {
-		return true;
+		return wordSet.contains(w);
 	}
 	
 	public void clear() {
+		wordSet.clear();
 	}
 	
 	public Iterator<Word> iterator() {
-		return null;
+		return wordSet.iterator();
 	}
 	
 	public int size() {
-		return 0;
+		return wordSet.size();
 	}
 }
