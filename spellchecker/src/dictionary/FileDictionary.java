5e68e731c23ede38c723e30d26c98af9240dafcc
Nicolás Bonader <neb0113@famaf.unc.edu.ar>
2015-05-23 23:21:15 -0300

    dict listo

--- spellchecker/src/dictionary/FileDictionary.java
+++ spellchecker/src/dictionary/FileDictionary.java
@@ -1,53 +1,67 @@
 package dictionary;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 import word.*;
-import dictionary.*;
 
 public class FileDictionary extends Dictionary {
 	
 	private String filePath;  
 
-	
 	public FileDictionary() {
 		load("dict.txt");
 	}
 
 	public FileDictionary(String str) {
-
 		load(str);
 	}
 	
-	public void load(String str) {
+	public void load(String path) {
 		String line;
-		boolean validLine;
-
+		int i = 0;
+		
+		/* guardar archivo origen */
+		this.filePath = path;
+		
 		/* intentamos cargar el archivo, linea por linea */
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(str));
+			BufferedReader br = new BufferedReader(new FileReader(path));
 			
-			/* si la linea es invalida, la salteamos, else, añadimos la palabra */
+			/* agregamos las lineas validas */
 			while((line = br.readLine()) != null) {
-				validLine = true;
-				for(int i = 0; i < line.length(); i++) {
-					validLine = validLine && Character.isLetter(line.charAt(i));
-				}
-				if(!validLine)
-					continue;
-				this.add(new Word(line));
+
+				if(line.matches("[a-zA-ZáéíóúüÁÉÍÓÚÜ]+"))
+					this.add(new Word(line));
 			}
+
+			br.close();
 		}catch (IOException e) {
 			System.out.println("No se pudo abrir el diccionario especificado. Usando uno vacío.");
 		}
 	}
 	
 	public void save() {
+		/* usar archivo de origen por defecto */
 		save(this.filePath);
 	}
 
-	public void save(String str) {
+	public void save(String path) {
+	
+		List <String> myList;
+		PrintWriter writer;
+
+		/* guardar las palabras en path*/
+		try {
+			writer = new PrintWriter(path);
+			myList = this.toStringList();
+			for(String str : myList)
+				writer.println(str);
+			
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo guardar el diccionario.");
+		}
+
+	
 	}
 }
